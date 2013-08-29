1776395b141b2ae0d25c6e1a57b6841c57909278
Jack Kelly <jack-list@xlk.org.uk>
2013-08-29 17:59:43 +0100

    FeatureList removed. FeatureList.find_nearest() re-built as utils.find_nearest() using pandas.  Closes #6

---
+++ slicedpy/utils.py
@@ -0,0 +1,38 @@
+from __future__ import division, print_function
+import pandas as pd
+import numpy as np
+
+def find_nearest(data, target, align='front', max_time_diff=None):
+    """Finds the index of the nearest row in `data` to `target` time.
+
+    Args:
+      * data (pd.Series or pd.DataFrame): if `align==back` then `data` must
+        have an `end` column.
+      * target (pd.Timeseries or timestamp)
+      * align (str): `front` or `back`.  Align with the front of the event
+        (as recorded by `data.index`) or the back (as recorded by `data['end']`)
+      * max_time_diff (datetime.timedelta or None): optional.
+
+    Returns:
+      int. Index into `data` for the element nearest in time to `target`.
+    
+    """
+
+    assert(align in ['front','back'])
+
+    if isinstance(target, pd.Timestamp):
+        target = target.to_pydatetime()
+
+    if align == 'front':
+        diff = data.index.to_pydatetime() - target
+    else:
+        diff = data['end'].values - target
+
+    diff = np.abs(diff)
+    min_diff = diff.min()
+    
+    if max_time_diff is not None and min_diff > max_time_diff:
+        return None
+    else:
+        return diff.argmin()
+    
