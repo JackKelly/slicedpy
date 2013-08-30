Modified Normal class so it can be used with DataStore class (i.e. added methods .fit(), partial_fit(), score()). Implemented correct “on-line” algorithm for updating variance and used this in Normal.partial_fit().

slicedpy/datastore.py
@@ -91,7 +91,7 @@ def test_fitting_and_plotting():
     from sklearn import mixture
     import matplotlib.pyplot as plt
 
-    model = mixture.GMM(n_components=2)
+    model = mixture.GMM(n_components=1)
     ds = DataStore(model=model)
     ds.append(np.random.randn(100,1))
     ds.append(10+np.random.randn(200,1))

slicedpy/normal.py
@@ -2,36 +2,79 @@ from __future__ import division, print_function
 import math
 import scipy.stats as stats
 import numpy as np
+from scipy.stats import norm
 from slicedpy.bunch import Bunch
 
 class Normal(Bunch):
     """
     Attributes:
-      * min
-      * max
-      * mean
-      * var (variance)
-      * size
+      (see reset())
     """
 
-    def __init__(self, values=None, **kwds):
+    def __init__(self, **kwds):
         """
         Args
           * values (np.ndarray) (optional)
         """
         super(Normal, self).__init__(**kwds)
-        if values is not None:
-            if not isinstance(values, np.ndarray):
-                raise TypeError('values must be an np.ndarray')
-            else:
-                self.size = values.size
-                self.var = values.var(ddof=1)
-                self.mean = values.mean()
-                self.min = values.min()
-                self.max = values.max()
+        self.reset()
+
+    def reset(self):
+        self._fit_has_been_called = False
+        self.min = None
+        self.max = None
+        self.mean = None
+        self.var = None
+        self.size = 0
+        self._M2 = 0
+        return self
+
+    @property
+    def std(self):
+        return math.sqrt(self.var)
+
+    def fit(self, values):
+        _sanity_check_values(values)
+        self._fit_has_been_called = True
+        self.size = values.size
+        self.var = values.var(ddof=1)
+        self.mean = values.mean()
+        self.min = values.min()
+        self.max = values.max()
+        return self
+
+    def partial_fit(self, values):
+        # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm
+        if self._fit_has_been_called:
+            raise Exception('You cannot call partial_fit() if fit() has been'
+                            ' called on this object!')
+
+        _sanity_check_values(values)
+
+        if self.mean is None:
+            self.mean = 0
+
+        for x in values:
+            self.size += 1
+            delta = x - self.mean
+            self.mean += delta / self.size
+            self._M2 += delta * (x - self.mean)
+
+        self.var = self._M2 / (self.size - 1)
+
+        if self.min is None:
+            self.min = values.min()
+            self.max = values.max()
+        else:
+            self.min = min(values.min(), self.min)
+            self.max = max(values.max(), self.max)
+
+        return self
+
+    def score(self, x):
+        return norm.logpdf(x, loc=self.mean, scale=self.std)
 
     def welch_ttest(self, other):
-
         # http://en.wikipedia.org/wiki/Welch%27s_t_test
         # http://stackoverflow.com/questions/10038543/tracking-down-the-assumptions-made-by-scipys-ttest-ind-function
 
@@ -57,22 +100,29 @@ class Normal(Bunch):
                                     stats.t.cdf(-math.fabs(t_stat), df))
         return two_tailed_p_value
 
-
     def similar_mean(self, other, p_value_threshold=0.1, mean_delta_threshold=5):
+        """Returns True is self and other have similar means."""
         return (math.fabs(self.mean - other.mean) < mean_delta_threshold or
                 self.welch_ttest(other) > p_value_threshold)
 
-
-    def rough_mean_of_two_normals(self, other):
+    def rough_combination(self, other):
         """Note that this gives the correct answer for ``size`` and ``mean``
         but will give the incorrect answer for ``var`` if the variables are
         correlated!!
 
-        Does not alter self.  Instead returns new :class:`Normal`.
+        Does not alter self.  Instead returns new instance of :class:`Normal`.
         """
-        new_size = self.size + other.size
-        new_mean = ((self.mean * self.size) + (other.mean * other.size)) / new_size
+        new = Normal()
+        new.size = self.size + other.size
+        new.mean = ((self.mean * self.size) + (other.mean * other.size)) / new.size
         # http://www.emathzone.com/tutorials/basic-statistics/combined-variance.html
-        new_var = (((self.size * (self.var + (self.mean - new_mean)**2)) +
-                    (other.size * (other.var + (other.mean - new_mean)**2))) / (new_size+2))
-        return Normal(size=new_size, mean=new_mean, var=new_var)
+        new.var = (((self.size * (self.var + (self.mean - new.mean)**2)) +
+                    (other.size * (other.var + (other.mean - new.mean)**2))) / (new.size+2))
+        new.min = min(self.min, other.min)
+        new.max = max(self.max, other.max)
+        return new
+
+
+def _sanity_check_values(values):
+    if not isinstance(values, np.ndarray):
+        raise TypeError('values must be an np.ndarray')

slicedpy/tests/test_normal.py
@@ -46,12 +46,13 @@ def two_samples_from_different_distribtions():
                       21.78256937,  15.31409412,   9.22848329,   2.44450088])
     return rvs1, rvs2
 
+
 class TestStats(unittest.TestCase):
 
     def test_welch_ttest(self):
         def _test_welch_ttest(rvs1, rvs2):
-            norm1 = Normal(values=rvs1)
-            norm2 = Normal(values=rvs2)
+            norm1 = Normal().fit(rvs1)
+            norm2 = Normal().fit(rvs2)
             wtt = norm1.welch_ttest(norm2)
             stt = stats.ttest_ind(rvs1, rvs2, equal_var=False)[1]
             self.assertAlmostEqual(wtt, stt)
@@ -63,29 +64,60 @@ class TestStats(unittest.TestCase):
 
     def test_similar_mean(self):
         rvs1, rvs2 = two_samples_from_same_distribtion()
-        norm1 = Normal(values=rvs1)
-        norm2 = Normal(values=rvs2)
+        norm1 = Normal().fit(rvs1)
+        norm2 = Normal().fit(rvs2)
         self.assertTrue(norm1.similar_mean(norm2))
 
         rvs1, rvs2 = two_samples_from_different_distribtions()
-        norm1 = Normal(values=rvs1)
-        norm2 = Normal(values=rvs2)
+        norm1 = Normal().fit(rvs1)
+        norm2 = Normal().fit(rvs2)
         self.assertFalse(norm1.similar_mean(norm2))
+
+    def _compare_normal_and_array(self, norm, arr, ddof=1):
+        self.assertEqual(norm.size, arr.size)
+        self.assertEqual(norm.max, arr.max())
+        self.assertEqual(norm.min, arr.min())
+        self.assertAlmostEqual(norm.mean, arr.mean())
+        self.assertAlmostEqual(norm.std, arr.std(ddof=ddof), 0)
+        self.assertEqual(round(norm.var), round(arr.var(ddof=ddof)))
 
     def test_rough_mean_of_two_normals(self):
         def _test_mean_of_two_normals(rvs1, rvs2):
             rvs3 = np.concatenate([rvs1, rvs2])
-            norm1 = Normal(values=rvs1)
-            norm2 = Normal(values=rvs2)
-            norm3 = norm1.rough_mean_of_two_normals(norm2)
-            self.assertEqual(norm3.size, rvs3.size)
-            self.assertAlmostEqual(norm3.mean, rvs3.mean())
-            self.assertEqual(round(norm3.var), round(rvs3.var()))
+            norm1 = Normal().fit(rvs1)
+            norm2 = Normal().fit(rvs2)
+            norm3 = norm1.rough_combination(norm2)
+            self._compare_normal_and_array(norm3, rvs3, ddof=0)
 
         rvs1, rvs2 = two_samples_from_same_distribtion()
         _test_mean_of_two_normals(rvs1, rvs2)
         rvs1, rvs2 = two_samples_from_different_distribtions()
         _test_mean_of_two_normals(rvs1, rvs2)
+
+    def test_reset(self):
+        rvs1, rvs2 = two_samples_from_different_distribtions()
+        norm = Normal().fit(rvs1)
+        self._compare_normal_and_array(norm, rvs1)
+        norm.reset()
+        norm.fit(rvs2)
+        self._compare_normal_and_array(norm, rvs2)
+
+    def test_partial_fit(self):
+        rvs1, rvs2 = two_samples_from_different_distribtions()
+        rvs3, rvs4 = two_samples_from_same_distribtion()
+        norm = Normal()
+        norm.partial_fit(rvs1)
+        self._compare_normal_and_array(norm, rvs1)
+        norm.partial_fit(rvs2)
+        arr = np.append(rvs1, rvs2)
+        self._compare_normal_and_array(norm, arr)
+        norm.partial_fit(rvs3)
+        arr = np.append(arr, rvs3)
+        self._compare_normal_and_array(norm, arr)
+        norm.partial_fit(rvs4)
+        arr = np.append(arr, rvs4)
+        self._compare_normal_and_array(norm, arr)
+        
 
 if __name__ == '__main__':
     unittest.main()