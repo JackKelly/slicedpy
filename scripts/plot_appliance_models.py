a6dce5c59b6ff8c05725bb5dc4ff2a0533bf5fc4
Jack Kelly <jack-list@xlk.org.uk>
2013-10-02 12:47:40 +0100

    much tidier code

--- scripts/plot_appliance_models.py
+++ scripts/plot_appliance_models.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 from pda.channel import Channel
 from slicedpy.appliance import Appliance
 import matplotlib.pyplot as plt
@@ -5,47 +6,38 @@ import os.path as path
 
 DATA_DIR = '/data/mine/domesticPowerData/BellendenRd/wattsUp'
 
-######### WASHING MACHINE
-wm_app = Appliance('wm')
+def train_appliance(label, sig_data_filenames):
+    """
+    Args:
+      * label (str): e.g. 'washing machine'
+      * sig_data_filenames (list of strings): filenames of signature data files
+    """
+    app = Appliance(label)
+    
+    for f_name in sig_data_filenames:
+        print('Loading', f_name)
+        chan = Channel()
+        chan.load_wattsup(path.join(DATA_DIR, f_name))
+        sps = app.train_on_single_example(chan)
 
-print('Loading washingmachine1.csv')
-wm1 = Channel()
-wm1.load_wattsup(path.join(DATA_DIR, 'washingmachine1.csv'))
-wm_app.train_on_single_example(wm1)
+    # plotting
+    fig1, ax1 = plt.subplots()
 
-print('Loading washingmachine2.csv')
-wm2 = Channel()
-wm2.load_wattsup(path.join(DATA_DIR, 'washingmachine2.csv'))
-wm_sig_power_states = wm_app.train_on_single_example(wm2)
+    chan.plot(ax1, date_format='%H:%M:%S')
+    ax1.set_title(f_name)
+    for ps in sps: # power segment in signature power segment
+        ps.plot(ax1)
 
-fig1, ax1 = plt.subplots()
-wm2.plot(ax1)
-for ps in wm_sig_power_states:
-    ps.plot(ax1)
+    app.draw_power_state_graph()
 
-wm_app.draw_power_state_graph()
+    for node in app.power_state_graph.nodes():
+        print(node)
+        print("node", node.power.get_model().mean, "essential=", node.essential)
 
-for node in wm_app.power_state_graph.nodes():
-    print(node)
-    print("node", node.power.get_model().mean, "essential=", node.essential)
 
-######### TV
-# tv_app = Appliance('tv')
-# tv1 = Channel()
-# tv1.load_wattsup(path.join(DATA_DIR, 'tv1.csv'))
-# tv_app.train_on_single_example(tv1)
-
-######## Toaster
-# toaster_app = Appliance('toaster')
-# t1 = Channel()
-# t1.load_wattsup(path.join(DATA_DIR, 'toaster1.csv'))
-# toaster_app.train_on_single_example(t1)
-
-######## Kettle
-# kettle_app = Appliance('kettle')
-# k1 = Channel()
-# k1.load_wattsup(path.join(DATA_DIR, 'kettle1.csv'))
-# kettle_app.train_on_single_example(k1)
-
+# train_appliance('washing machine', ['washingmachine1.csv', 'washingmachine2.csv'])
+train_appliance('tv', ['tv1.csv'])
+# train_appliance('toaster', ['toaster1.csv'])
+# train_appliance('kettle', ['kettle1.csv'])
 
 plt.show()
